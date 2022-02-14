0c32ba5edbe6ca770ce5e14c87a2f9a69efdb161
nohehf <nohe.hinniger.foray@gmail.com>
2022-02-14 17:13:43 +0100

    serve files

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,54 @@
 import {
   Controller,
+  Get,
+  NotFoundException,
+  Param,
   Post,
+  Response,
+  StreamableFile,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { createReadStream, existsSync } from 'fs';
+import { diskStorage } from 'multer';
 
-@Controller('files')
+@Controller('file')
 export class FilesController {
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads',
+      }),
+    }),
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    return {
+      originalName: file.originalname,
+      newName: file.filename,
+      path: '/file/' + file.filename,
+    };
   }
+
+  @Get(':id')
+  getFile(
+    @Param('id') id: string,
+    @Response({ passthrough: true }) res,
+  ): StreamableFile {
+    const filepath: string = './uploads/' + id;
+    if (existsSync(filepath)) {
+      const file = createReadStream(filepath);
+      res.set({
+        'Content-Disposition': `inline; filename="${id}.pdf"`,
+        'Content-Type': 'application/pdf',
+      });
+      return new StreamableFile(file);
+    }
+    throw new NotFoundException();
+  }
+}
+
+async function saveFile(file: Express.Multer.File): Promise<any> {
+  return;
 }
--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 
+//imports:  //[MulterModule.register({ dest: './src/files/uploads' })],
 @Module({
   controllers: [FilesController],
   providers: [FilesService],
